e5003be10eabd5298c60366d68ac1a5f14529b9e
Mikheyev_Denis <mikheydd@mail.ru>
2022-11-12 15:07:19 +0300

    DZ task4

---
+++ task4_max_from_3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число третье число: ");
+int C = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (A > max) 
+{
+    max = A;
+};
+if (B > max) 
+{
+    max = B;
+};
+if (C > max) 
+{
+    max = C;
+};
+Console.Write("max = ");
+Console.Write(max);
