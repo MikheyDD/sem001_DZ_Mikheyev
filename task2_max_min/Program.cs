fc0d477c86eb1b9c94801cbfa903684755aa7ebc
Mikheyev_Denis <mikheydd@mail.ru>
2022-11-12 14:50:38 +0300

    DZ task2

---
+++ task2_max_min/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+if (A == B)
+{
+    Console.WriteLine("Числа равны");
+} else
+if (A > B) 
+{
+    Console.Write("max = ");
+    Console.Write(A);
+    Console.Write(" , min = ");
+    Console.Write(B);
+}
+else 
+{
+Console.Write("max = ");
+    Console.Write(B);
+    Console.Write(" , min = ");
+    Console.Write(A);
+}
