c837047cbb30f044e23c7400f9112f9f2138e629
Mikheyev_Denis <mikheydd@mail.ru>
2022-11-13 20:38:39 +0300

    task8 Четные числа от 1 до N

---
+++ task8_N/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число : ");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = 1;
+while (B <= A)
+{
+    if (B % 2 == 0)
+    {
+        Console.Write(B + ", ");
+    }
+    B++;
+}
