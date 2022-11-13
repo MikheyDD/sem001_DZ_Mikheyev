7a3891acdeceaf144d6f885e2cb5676131af5ae3
Mikheyev_Denis <mikheydd@mail.ru>
2022-11-13 12:29:26 +0300

    DZ task4 синтаксические исправления

--- task4_max_from_3/Program.cs
+++ task4_max_from_3/Program.cs
@@ -1,21 +1,11 @@
-﻿Console.WriteLine("Введите число первое число: ");
+﻿Console.WriteLine("Введите первое число: ");
 int A = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число второе число: ");
+Console.WriteLine("Введите второе число: ");
 int B = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число третье число: ");
+Console.WriteLine("Введите третье число: ");
 int C = Convert.ToInt32(Console.ReadLine());
-int max = 0;
-if (A > max) 
-{
-    max = A;
-};
-if (B > max) 
-{
-    max = B;
-};
-if (C > max) 
-{
-    max = C;
-};
+int max = A;
+if (B > max) max = B;
+if (C > max)  max = C;
 Console.Write("max = ");
 Console.Write(max);
